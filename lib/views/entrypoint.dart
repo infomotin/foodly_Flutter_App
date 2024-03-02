5db8dc9ea9a09740873684381ee7cb73a28a1600
Abdul Motin <infomotin@users.noreply.github.com>
2024-03-02 15:26:30 +0600

    Controller Createing And Init With Main Screen With Bootom Create Page and Floder link page into Containner

--- lib/views/entrypoint.dart
+++ lib/views/entrypoint.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:foodly/constants/constants.dart';
 import 'package:foodly/controllers/tab_index_controller.dart';
+import 'package:foodly/views/cart/cart_page.dart';
+import 'package:foodly/views/home/home_page.dart';
+import 'package:foodly/views/profile/profile_page.dart';
+import 'package:foodly/views/search/search_page.dart';
 import 'package:get/get.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 
 // ignore: must_be_immutable
 class MainScreen extends StatelessWidget {
   MainScreen({super.key});
-    List<Widget> pages = [];
+    List<Widget> pages = const [
+      HomePage(),
+      SearchPage(),
+      CartPage(),
+      ProfilePage(),
+    ];
   
   @override
   Widget build(BuildContext context) {
@@ -16,11 +25,7 @@ class MainScreen extends StatelessWidget {
     return Obx(() => Scaffold(
           body: Stack(
             children: [
-              Container(  
-                height: height,
-                width: width,
-                color: kOffWhite,
-              ),
+              pages[controller.tabIndex],
               Align(
                 alignment: Alignment.bottomCenter,
                 child: Theme(
