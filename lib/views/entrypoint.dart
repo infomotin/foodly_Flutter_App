a35bd078f4c2d3c43a282e8d5d619e086331b6bf
Abdul Motin <infomotin@users.noreply.github.com>
2024-03-02 15:07:22 +0600

    Controller Createing And Init With Main Screen With Bootom Create Page and Floder

--- lib/views/entrypoint.dart
+++ lib/views/entrypoint.dart
@@ -2,45 +2,62 @@ import 'package:flutter/material.dart';
 import 'package:foodly/constants/constants.dart';
 import 'package:foodly/controllers/tab_index_controller.dart';
 import 'package:get/get.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 
+// ignore: must_be_immutable
 class MainScreen extends StatelessWidget {
-  const MainScreen({super.key});
-
+  MainScreen({super.key});
+    List<Widget> pages = [];
+  
   @override
   Widget build(BuildContext context) {
     //caling controller tabIndex
     final controller = Get.put(TabIndexController());
-    return  Obx(() => Scaffold(
-        body: Stack(
-      children: [
-        Container(
-          height: height,
-          width: width,
-          color: kOffWhite,
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Theme(
-            data: Theme.of(context).copyWith(canvasColor: kPrimary),
-            child: BottomNavigationBar(
-              showSelectedLabels:false,
-              showUnselectedLabels: false,
-              unselectedIconTheme: IconThemeData(color: Colors.black38),
-              selectedIconTheme: IconThemeData(color: Colors.red),
-              onTap: (value) {
-                // Respond to item press.      
-               controller .setTabIndex = value;
-              },
-              currentIndex: controller .tabIndex,
-              items: const [
-                BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-                BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-                BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Cart'),
-                BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),],
-            )),
-        )
-      ],
-    ),
-    ));
+    return Obx(() => Scaffold(
+          body: Stack(
+            children: [
+              Container(  
+                height: height,
+                width: width,
+                color: kOffWhite,
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Theme(
+                    data: Theme.of(context).copyWith(canvasColor: kPrimary),
+                    child: BottomNavigationBar(
+                      showSelectedLabels: false,
+                      showUnselectedLabels: false,
+                      unselectedIconTheme: IconThemeData(color: Colors.black38),
+                      selectedIconTheme: IconThemeData(color: Colors.red),
+                      onTap: (value) {
+                        // Respond to item press.
+                        controller.setTabIndex = value;
+                      },
+                      currentIndex: controller.tabIndex,
+                      items: [
+                        BottomNavigationBarItem(
+                            icon: controller.tabIndex == 0
+                                ? Icon(AntDesign.appstore1)
+                                : Icon(AntDesign.appstore_o),
+                            label: 'Home'),
+                        BottomNavigationBarItem(
+                            icon: Icon(Icons.search), label: 'Search'),
+                        BottomNavigationBarItem(
+                            icon: Badge(
+                                label: Text('1'),
+                                child: Icon(FontAwesome.opencart)),
+                            label: 'Cart'),
+                        BottomNavigationBarItem(
+                            icon: controller.tabIndex == 3
+                                ? Icon(FontAwesome.user_circle)
+                                : Icon(FontAwesome.user_circle_o),
+                            label: 'Profile'),
+                      ],
+                    )),
+              )
+            ],
+          ),
+        ));
   }
 }
